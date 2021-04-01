b315f9a81764149bc519ab2a47bac1c2a80d8c84
Brian Dillingham <brian@dillingham.dev>
2021-04-01 04:54:02 -0400

    wip

--- resources/views/create/image.blade.php
+++ resources/views/create/image.blade.php
@@ -1,1 +1,5 @@
-<jet-image-input id="{{ $field->snake() }}" v-model="form.{{ $field->snake()  }}" class="mt-1 block w-full" />
+<div class="col-span-6 sm:col-span-4">
+    <jet-label for="{{ $field->snake() }}" value="{{ $field->studly() }}" />
+    <jet-image-input id="{{ $field->snake() }}" v-model="form.{{ $field->snake()  }}" class="mt-1 block w-full" />
+    <jet-input-error :message="form.errors.{{ $field->snake() }}" class="mt-2" />
+</div>
--- resources/views/edit/image.blade.php
+++ resources/views/edit/image.blade.php
@@ -1,1 +1,1 @@
-<jet-image-input id="{{ $field->snake() }}" v-model="form.{{ $field->snake()  }}" class="mt-1 block w-full" />
+@include('stubkit::create.image')
